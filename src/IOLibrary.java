f0e64247a6b43df2956e8dee7d29fa0f1b32e5ae
oshnikm <106856676+oshnikm@users.noreply.github.com>
2022-06-05 01:03:55 +0530

    Add files via upload

---
+++ src/IOLibrary.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public final class IOLibrary {
+private IOLibrary() {
+	throw new IllegalStateException("Utility Class");
+}		
+  public static String getString() {
+    BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    do {
+      try {
+        return r.readLine();
+      } catch (Exception e) {
+      }
+    } while (true);
+  }
+
+  public static InetAddress getIPAddress() {
+    BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    do {
+      try {
+        String[] chunks = r.readLine().split("\\.");
+        byte[] data = { Byte.parseByte(chunks[0]),Byte.parseByte(chunks[1]),Byte.parseByte(chunks[2]),Byte.parseByte(chunks[3])};
+        return Inet4Address.getByAddress(data);
+      } catch (Exception e) {
+      }
+    } while (true);
+  }
+
+}
---
+++ src/Location.java
@@ -0,0 +1,28 @@
+
+public class Location extends SpacePlace {
+  public int c;
+  public int r;
+  public DIRECTION d;
+  
+  public enum DIRECTION {VERTICAL, HORIZONTAL};
+  
+  public Location(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
+
+  public Location(int r, int c, DIRECTION d) {    
+    this(r,c);
+    this.d=d;
+  }
+  
+  public String toString() {
+    if(d==null){
+    	return new StringBuilder().append("(").append(c+1).append(",")
+    			.append(r+1).append(")").toString();
+    } else {
+    	return new StringBuilder().append("(").append(c+1).append(",")
+    			.append(r+1).append(")").toString();
+    }
+  }
+}
